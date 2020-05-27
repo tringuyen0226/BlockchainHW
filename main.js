09bcb6598d6f56f3e74aafe02493768df6e70265
tri <thanhtri7637@gmail.com>
2020-05-27 23:53:02 +0700

    Edit main.js
    Fix blockchain created
    Add isChainValid to check chain

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ class Block {
     }
 
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data).toString());
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
     }
 
 }
@@ -18,15 +18,15 @@ class Block {
 class Blockchain {
 
     constructor() {
-        this.chain = [this.createGenesisBlock()];
+        this.chain = [this.createFirstBlock()];
     }
 
-    createGenesisBlock() {
-        return new Block(0,'25/05/2020','Constructor Block','0');
+    createFirstBlock() {
+        return new Block(0,'25/05/2020','First Block','0');
     }
 
     getLatestBlock() {
-        return this.chain[this.chain.length-1];
+        return this.chain[this.chain.length - 1];
     }
 
     addBlock(newBlock) {
@@ -35,10 +35,36 @@ class Blockchain {
         this.chain.push(newBlock);
     }
 
+    isChainValid() {
+        for(let i = 1; i < this.chain.length ;i++)
+        {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
+
+            if(currentBlock.hash !== currentBlock.calculateHash()) {
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash)
+            {
+                return false;
+            }
+        }
+
+        return true;
+
+    }
 }
 
 let wallet = new Blockchain();
-wallet.addBlock(new Block(1,'27/05/2020',{amount: 5}));
-wallet.addBlock(new Block(2,'27/05/2020',{amount: 10}));
+wallet.addBlock(new Block(1,"24/05/2020",{amount: 4}));
+wallet.addBlock(new Block(2,"25/07/2020",{amount: 10}));
 
-console.log(JSON.stringify(wallet,null,4));
+console.log('Is valid blockchain ?' + wallet.isChainValid());
+
+wallet.chain[1].data = {amount: 100};
+wallet.chain[1].hash = wallet.chain[1].calculateHash();
+
+console.log('Is valid blockchain ?' + wallet.isChainValid());
+
+// console.log(JSON.stringify(wallet,null,4));
